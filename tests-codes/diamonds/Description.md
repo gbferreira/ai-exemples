d59b6b4d8745a0d25d8bf1d1f114f1cfb48f4794
gbferreira <gabrielferreira241196@gmail.com>
2026-05-15 16:54:37 -0300

    feat: Added diamonds exercice

---
+++ tests-codes/diamonds/Description.md
@@ -0,0 +1,9 @@
+You need create a function that return this pattern in console.
+
+diamonds(l, c, s);
+
+This function has theses inputs:
+l = lines;
+c = colummns;
+s = size;
+
---
+++ tests-codes/diamonds/index.js
@@ -0,0 +1,8 @@
+/**
+ * Entry point — implement diamond printing logic here.
+ */
+function diamonds() {
+  console.log("Run `npm start` after implementing diamonds.");
+}
+
+diamonds();
