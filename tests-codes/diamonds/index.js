aa84f0e80cd0c52f8a8452ce8ba83d15346cd2da
gbferreira <gabrielferreira241196@gmail.com>
2026-05-15 17:06:00 -0300

    docs: added new exercise

--- tests-codes/diamonds/index.js
+++ tests-codes/diamonds/index.js
@@ -1,8 +1,18 @@
-/**
- * Entry point — implement diamond printing logic here.
- */
-function diamonds() {
-  console.log("Run `npm start` after implementing diamonds.");
+
+function isPotisitionToDot()
+
+function diamonds(l, c, s) {
+  for (let i = 0; i < l; i++) {
+    for (let j = 0; j < c; j++) {
+        if(isPotisitionToDot(i, j, l, c, s)) {
+            console.log('.');
+        } else if(isPotisitionToSlash(i, j, l, c, s)) {
+            console.log('/');
+        } else {
+            console.log('\\');
+        }
+    }
+  }
 }
 
-diamonds();
+diamonds(3, 1, 2);
